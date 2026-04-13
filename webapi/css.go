2df04fe558ca25ef09d8c1446d3f097c66eee326
codeation <39458623+codeation@users.noreply.github.com>
2026-04-13 15:54:55 -0700

    Simplify code

--- webapi/css.go
+++ webapi/css.go
@@ -4,32 +4,58 @@ import (
 	"fmt"
 )
 
-var styleName = map[int]string{
-	0: "normal",
-	1: "oblique",
-	2: "italic",
+func styleName(code int) string {
+	switch code {
+	case 0:
+		return "normal"
+	case 1:
+		return "oblique"
+	case 2:
+		return "italic"
+	default:
+		return "normal"
+	}
 }
 
-var variantName = map[int]string{
-	0: "normal",
-	1: "small-caps",
+func variantName(code int) string {
+	switch code {
+	case 0:
+		return "normal"
+	case 1:
+		return "small-caps"
+	default:
+		return "normal"
+	}
 }
 
-var stretchName = map[int]string{
-	0: "ultra-condensed",
-	1: "extra-condensed",
-	2: "condensed",
-	3: "semi-condensed",
-	4: "normal",
-	5: "semi-expanded",
-	6: "expanded",
-	7: "extra-expanded",
-	8: "ultra-expanded",
+func stretchName(code int) string {
+	switch code {
+	case 0:
+		return "ultra-condensed"
+	case 1:
+		return "extra-condensed"
+	case 2:
+		return "condensed"
+	case 3:
+		return "semi-condensed"
+	case 4:
+		return "normal"
+	case 5:
+		return "semi-expanded"
+	case 6:
+		return "expanded"
+	case 7:
+		return "extra-expanded"
+	case 8:
+		return "ultra-expanded"
+	default:
+		return "normal"
+	}
 }
 
 func fontValue(height int, style, variant, weight, stretch int, family string) string {
 	return fmt.Sprintf("%s %s %d %s %dpx %s",
-		styleName[style], variantName[variant], weight, stretchName[stretch], height, family)
+		styleName(style), variantName(variant), weight, stretchName(stretch), height, family)
 }
 
 func color(r, g, b, a uint16) string {
